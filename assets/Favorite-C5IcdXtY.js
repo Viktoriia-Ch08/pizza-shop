444c8185fc192c901a2d7c06dde3f17b3b4320a7
Viktoriia-Ch08 <112558987+Viktoriia-Ch08@users.noreply.github.com>
2024-03-14 21:24:07 +0000

    Deploying to gh-pages from @ Viktoriia-Ch08/pizza-shop@a756d6ff9e856fd6bf1df4922d9e69da2e8e4f55 🚀

---
+++ assets/Favorite-C5IcdXtY.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DKPPHdM3.js";const o=()=>t.jsx("button",{children:"Favorite"});export{o as default};
---
+++ assets/Home-vYs5-L9G.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DKPPHdM3.js";const e=()=>t.jsx("button",{children:"Home"});export{e as default};
