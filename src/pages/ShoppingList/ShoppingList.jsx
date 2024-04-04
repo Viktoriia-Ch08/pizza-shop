3700be4d591b259bb808df14f2b9b96b592696b0
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2024-04-04 21:02:24 +0200

    created new page, added deletedFromOrder fnc

--- src/pages/ShoppingList/ShoppingList.jsx
+++ src/pages/ShoppingList/ShoppingList.jsx
@@ -1,5 +1,51 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectOrder } from "../../redux/selectors";
+import { deleteFromOrder } from "../../redux/pizzasSlice";
+
 const ShoppingList = () => {
-  return <button>Shopping</button>;
+  const order = useSelector(selectOrder);
+  const dispatch = useDispatch();
+
+  const deleteFromOrderList = (id) => {
+    const pizzaToDelete = order.find((pizza) => pizza.id === id);
+    if (pizzaToDelete !== undefined) {
+      dispatch(deleteFromOrder(pizzaToDelete));
+      return;
+    }
+  };
+
+  return (
+    <div>
+      <ul>
+        {order.length > 0 &&
+          order.map(
+            ({ name, description, type, price, imageUrl, toppings, id }) => {
+              return (
+                <li>
+                  <img src={imageUrl} alt={name} />
+                  <h2>{name}</h2>
+                  <p>{description}</p>
+                  <p>{type}</p>
+                  <p>{price}</p>
+                  <ul>
+                    {toppings.map((topping) => {
+                      return (
+                        <li>
+                          <p>{topping.name}</p>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <button type="button" onClick={() => deleteFromOrderList(id)}>
+                    delete from cart
+                  </button>
+                </li>
+              );
+            }
+          )}
+      </ul>
+    </div>
+  );
 };
 
 export default ShoppingList;
